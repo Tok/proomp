0fa2eab6c7b1569b9433519832a184d9ecbf5995
Tok <lsteiger4@gmail.com>
2022-12-18 16:06:59 +0100

    Cambium logger added.

--- src/proomp/config.clj
+++ src/proomp/config.clj
@@ -1,14 +1,19 @@
 (ns proomp.config
-  (:require [libpython-clj2.python :as py]))
+  (:require [cambium.core :as log]
+            [libpython-clj2.python :as py]))
 
-(println "Loading config.")
+(log/info "Loading config.")
 
 (def ^:private workspace-path "C:\\Users\\Zir\\Documents\\workspace\\proomp\\")
 (def model-path (str workspace-path "models\\stable-diffusion-2-1\\"))
 (def media-path (str workspace-path "generated-media\\"))
+(log/debug {:model-path model-path})
+(log/debug {:media-path media-path})
 
 (def ^:private python-dir "\\Users\\Zir\\AppData\\Local\\Programs\\Python\\")
+(log/trace {:python-dir python-dir})
+
 (py/initialize!
   :library-path (str python-dir "Python39\\python39.dll")
-  :python-executable (str python-dir"Python39\\python.exe")
+  :python-executable (str python-dir "Python39\\python.exe")
   :python-verbose false)
--- src/proomp/core.clj
+++ src/proomp/core.clj
@@ -1,6 +1,7 @@
 (ns proomp.core
   (:require [proomp.config :as config]
             [proomp.constants :as const]
+            [cambium.core :as log]
             [libpython-clj2.require :refer [require-python]]
             [libpython-clj2.python :refer [py. py.-] :as py]))
 (require-python 'torch '[torch.cuda :as cuda] 'transformers)
@@ -8,7 +9,7 @@
 
 (defn -main []
   (cuda/empty_cache)
-  (println "Creating pipeline.")
+  (log/debug "Creating pipeline.")
   (let [file-name (str config/media-path const/prompt ".png")
         pipe (py. StableDiffusionPipeline "from_pretrained" config/model-path)
         pipe-scheduler (py.- pipe "scheduler")
@@ -16,12 +17,11 @@
         dpm-scheduler (py. DPMSolverMultistepScheduler "from_config" pipe-scheduler-config)]
     (py/set-attr! pipe "scheduler" dpm-scheduler)
     (py. pipe "to" "cuda")
-    (println "Enabling attention slicing.")
     (py. pipe "enable_attention_slicing")
-    (println "Generating" file-name)
+
+    (log/info {:prompt const/prompt})
+    (log/debug {:file-name file-name})
     (let [result (py/$c pipe const/prompt)
           images (py.- result "images")
           image (nth images 0)]
-      (py. image "save" file-name)
-      (newline)
-      (println "Done."))))
+      (py. image "save" file-name))))
